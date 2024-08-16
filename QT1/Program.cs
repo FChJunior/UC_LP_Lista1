8c0c0dc20a6ccef49b895fbd7ff3cc7a33132a47
ChagasJunior <jniors75@gmail.com>
2024-08-16 11:59:11 -0300

    Iniciando o Repo

---
+++ QT1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*-------------------------------------------------------------------
+Questão 1: Informações do Personagem
+• Contextualização: Em um jogo de RPG, o jogador precisa inserir 
+  as informações básicas do personagem antes de começar a aventura.
+• Comando: Crie um programa que receba o
+
+@Lista: 01 - Lógica de Programação
+@Autor: Chagas Junior
+@Data: 16/08/2024
+---------------------------------------------------------------------*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
