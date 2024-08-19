7215df51166d37d8627151598cacb19719474e1d
ChagasJunior <jniors75@gmail.com>
2024-08-19 15:25:01 -0300

    Questão 9: Iniciando

--- QT6/obj/Debug/net8.0/QT6.AssemblyInfo.cs
+++ QT6/obj/Debug/net8.0/QT6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QT6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e74cf81cbda069f38e471b1bacaad36dcd7fe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5782be81deed714569523b9d1ad1b965d841c887")]
 [assembly: System.Reflection.AssemblyProductAttribute("QT6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QT6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- QT8/obj/Debug/net8.0/QT8.AssemblyInfo.cs
+++ QT8/obj/Debug/net8.0/QT8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QT8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2562410e2557bc12d6318c791a76a1a17f76090a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5782be81deed714569523b9d1ad1b965d841c887")]
 [assembly: System.Reflection.AssemblyProductAttribute("QT8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QT8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
