e92038bf361cdb46e8ee80a77f7b8dc40401b191
ChagasJunior <jniors75@gmail.com>
2024-08-19 15:16:02 -0300

    Questão 8: Iniciando

--- QT1/obj/Debug/net8.0/QT1.AssemblyInfo.cs
+++ QT1/obj/Debug/net8.0/QT1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QT1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676461e72cff67444b66578c115ef04fbf401490")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e74cf81cbda069f38e471b1bacaad36dcd7fe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("QT1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QT1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
