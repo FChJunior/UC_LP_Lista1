fa6869a6d8e9a6a144bdf4658729bf6d6c60e2f7
ChagasJunior <jniors75@gmail.com>
2024-08-19 07:38:36 -0300

    Questão 3: Iniciando

--- QT1/obj/Debug/net8.0/QT1.AssemblyInfo.cs
+++ QT1/obj/Debug/net8.0/QT1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QT1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a913a0094f960b6c94db9d1c6ef261a3ef8a79fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee8ec62542ce7beedf0d5cf52d29ac502d7a0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("QT1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QT1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- QT3/obj/Debug/net8.0/QT3.AssemblyInfo.cs
+++ QT3/obj/Debug/net8.0/QT3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("QT2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("QT3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee8ec62542ce7beedf0d5cf52d29ac502d7a0f6")]
-[assembly: System.Reflection.AssemblyProductAttribute("QT2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("QT2")]
+[assembly: System.Reflection.AssemblyProductAttribute("QT3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("QT3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
